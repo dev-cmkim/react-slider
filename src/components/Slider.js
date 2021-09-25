4b3a79c486cb341f2097670d35d79629562f723b
dev-cmkim <cm971003@gmail.com>
2021-09-25 22:36:36 +0900

    슬라이더 이전화면 이동 관련 수정필요 -> 전체적인 로직 수정

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -7,25 +7,30 @@ const Slider = () => {
     const [ImageNum, setImageNum] = useState(1);
 
 const nextSlide = () => {
-    if (currentSlide >= TOTAL_SLIDES) {
-        // 슬라이드 초기화
+    if (currentSlide === TOTAL_SLIDES) {
+        alert('마지막 페이지 입니다');
+    } else if (currentSlide >= TOTAL_SLIDES) {
         setCurrentSlide(0);
         setImageNum(1)
     } else {
         setCurrentSlide( currentSlide +1);
         setImageNum (ImageNum + 1);
-
     }
 };
 const prevSlide = () => {
-    if (currentSlide === 0) {
-        setCurrentSlide(TOTAL_SLIDES);
-    } else {
+    if (currentSlide === TOTAL_SLIDES) {
+        //마지막페이지
         setCurrentSlide(currentSlide -1);
-        setImageNum (ImageNum -1);
-    }
+        setImageNum (2);
+    } else if( currentSlide === 1 ) {
+        //두번째페이지
+        setCurrentSlide(currentSlide - 1);
+        setImageNum(1)
+    } else if (currentSlide === 0 ) {
+        alert('첫번째 페이지 입니다.')
+    } 
 };
-
+    console.log(ImageNum)
 
     return (
         <Container>
